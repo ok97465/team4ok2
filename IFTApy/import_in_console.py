5cd893a9f43f73644c097f6ced80508388ce244f
ok97465 <ok97465@gmail.com>
2025-06-12 14:58:16 -0400

    add_python

---
+++ IFTApy/import_in_console.py
@@ -0,0 +1,45 @@
+"""Init oklib package."""
+
+# Matlab command
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.pyplot import figure, hist, plot, subplots
+from mkl_fft import fft, fft2, ifft, ifft2
+from mkl_random import choice, randint, randn, standard_normal, uniform
+from numpy import (
+    amax,
+    amin,
+    angle,
+    arange,
+    arccos,
+    arcsin,
+    arctan,
+    argmax,
+    argmin,
+    array,
+    conj,
+    cos,
+    cross,
+    cumsum,
+    deg2rad,
+    diff,
+    dot,
+    exp,
+    hstack,
+    interp,
+    linspace,
+    mean,
+    newaxis,
+    ones,
+    pi,
+    rad2deg,
+    sin,
+    sqrt,
+    tan,
+    unwrap,
+    vstack,
+    zeros,
+)
+from numpy.fft import fftshift, ifftshift
+from numpy.linalg import norm, svd
+from scipy.special import cosdg, sindg, tandg
